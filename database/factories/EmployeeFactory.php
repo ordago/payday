9dba77ac0d45a9e57dab44c743a570995d182dee
Joó Martin <m4rt1n.j00@gmail.com>
2021-12-18 20:57:05 +0100

    Add job titles

--- database/factories/EmployeeFactory.php
+++ database/factories/EmployeeFactory.php
@@ -26,13 +26,18 @@ class EmployeeFactory extends Factory
         $hasSalary = !!rand(0, 1);
         $salary = $hasSalary ? rand(48000, 120000) * 100 : null;
         $hourlyRate = !$hasSalary ? rand(15, 150) * 100 : null;
+        $jobTitles = collect([
+            'Full stack developer',
+            'Frontend developer',
+            'Backend developer',
+        ]);
 
         return [
             'uuid' => $this->faker->uuid,
             'full_name' => $this->faker->name(),
             'email' => $this->faker->safeEmail,
             'department_id' => Department::factory(),
-            'job_title' => $this->faker->words(2, true),
+            'job_title' => $jobTitles->random(),
             'payment_type_class' => $this->faker->regexify('[A-Za-z0-9]{70}'),
             'salary' => $salary,
             'hourly_rate' => $hourlyRate,
