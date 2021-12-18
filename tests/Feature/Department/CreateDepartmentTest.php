9d6df48eacd5018fd304586f89dc4abb52b33709
Joó Martin <m4rt1n.j00@gmail.com>
2021-12-18 22:13:08 +0100

    Test department API

---
+++ tests/Feature/Department/CreateDepartmentTest.php
@@ -0,0 +1,41 @@
+<?php
+
+use App\Models\Department;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
+
+use function Pest\Laravel\postJson;
+
+it('should return 422 if name is invalid', function ($name) {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    Department::factory([
+        'name' => 'Development',
+    ])->create();
+
+    postJson(route('departments.store'), [
+        'name' => $name,
+        'description' => 'description'
+    ])->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+})->with([
+    '',
+    null,
+    'Development'
+]);
+
+it('should store a department', function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+
+    $department= postJson(route('departments.store'), [
+        'name' => 'Development',
+        'description' => 'Description'
+    ])->json('data');
+
+    expect($department)
+        ->attributes->toBe([
+            'name' => 'Development',
+            'description' => 'Description',
+        ]);
+});
