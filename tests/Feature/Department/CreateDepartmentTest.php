214f3578b7087b22b7d4972255510b5eadbf113b
Joó Martin <m4rt1n.j00@gmail.com>
2021-12-22 20:40:29 +0100

    Use assert invalid

--- tests/Feature/Department/CreateDepartmentTest.php
+++ tests/Feature/Department/CreateDepartmentTest.php
@@ -18,21 +18,21 @@ it('should return 422 if name is invalid', function (?string $name) {
 
     postJson(route('departments.store'), [
         'name' => $name,
-        'description' => 'description'
-    ])->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+        'description' => 'description',
+    ])->assertInvalid(['name']);
 })->with([
     '',
     null,
     'Development'
 ]);
 
-it('should store a department', function () {
-    $department= postJson(route('departments.store'), [
+it('should create a department', function () {
+    $department = postJson(route('departments.store'), [
         'name' => 'Development',
-        'description' => 'Description'
+        'description' => 'Awesome developers across the board!',
     ])->json('data');
 
     expect($department)
         ->attributes->name->toBe('Development')
-        ->attributes->description->toBe('Description');
+        ->attributes->description->toBe('Awesome developers across the board!');
 });
