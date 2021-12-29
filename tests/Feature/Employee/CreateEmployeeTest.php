a953b4df5177d9ecbeb7fcfffcc2bb4ba18f6d01
Joó Martin <m4rt1n.j00@gmail.com>
2021-12-29 15:30:42 +0100

    18 - Employee email validation tests

---
+++ tests/Feature/Employee/CreateEmployeeTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Department;
+use App\Models\Employee;
+use App\Models\User;
+
+use function Pest\Laravel\postJson;
+
+beforeEach(function () {
+    $user = User::factory()->create();
+    $this->actingAs($user);
+});
+
+it('should return 422 if the email is invalid', function (?string $email) {
+    Employee::factory([
+        'email' => 'taken@gmail.com',
+    ])->create();
+
+    postJson(route('employees.store'), [
+        'fullName' => 'Test Employee',
+        'email' => $email,
+        'departmentId' => Department::factory()->create()->uuid,
+        'jobTitle' => 'BE Developer',
+        'paymentType' => 'salary',
+        'salary' => 50000 * 100,
+    ])->assertInvalid(['email']);
+})->with([
+    '',
+    null,
+    'taken@gmail.com',
+]);
