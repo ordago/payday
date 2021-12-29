a4a5daebf6a9cf76817e1ea4ebdc71e32f77053c
Joó Martin <m4rt1n.j00@gmail.com>
2021-12-29 15:46:04 +0100

    20 - PaymentTypes enum

--- app/Enums/PaymentTypes.php
+++ app/Enums/PaymentTypes.php
@@ -2,21 +2,8 @@
 
 namespace App\Enums;
 
-use App\Models\Employee;
-use App\PaymentTypes\Concerns\PaymentType;
-use App\PaymentTypes\HourlyRate;
-use App\PaymentTypes\Salary;
-
 enum PaymentTypes: string
 {
     case SALARY = 'salary';
     case HOURLY_RATE = 'hourlyRate';
-
-    public function makePaymentType(Employee $employee): PaymentType
-    {
-        return match ($this) {
-            self::SALARY => new Salary($employee),
-            self::HOURLY_RATE => new HourlyRate($employee),
-        };
-    }
 }
